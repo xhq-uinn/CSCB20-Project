739cd079b28b3bf045b4b79898cdb3a8c7b260d3
xhq-uinn <jy.zhang668@gmail.com>
2026-04-07 21:54:47 -0400

    feat: login signup javascript

--- static/main.js
+++ static/main.js
@@ -9,4 +9,12 @@ sellbtn.addEventListener("click", function () {
     window.location.href = "/sell";
 });
 
+const logInBtn = document.getElementByID("login");
+logInBtn.addEventListener("click", function(){
+    window.location.href = "/login"
+});
 
+const signUpBtn = document.getElementByID("signup")
+signUpBtn.addEventListener("click", function(){
+    window.location.href = "/signup"
+});
